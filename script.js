78191850a23ab90d500824f1b9be92d13d1898e9
webtech2k16 <s.n.khalil@student.tue.nl>
2016-11-30 23:26:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-$(document).ready(function() {
     $('button').click(function() {
     	var toAdd = $("input[name=message]").val();
         $('#messages').append("<p>"+toAdd+"</p>");
     });
-});
